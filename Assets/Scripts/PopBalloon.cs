865f76afa3345ed7a42c26ab5b5b6e2d7f0a7187
marlaaaaine <98628128+marlaaaaine@users.noreply.github.com>
2024-01-23 22:22:03 -0500

    added script that handles when the popping particle plays and updated visual studio code package

--- Assets/Scripts/PopBalloon.cs
+++ Assets/Scripts/PopBalloon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PopBalloon : MonoBehaviour
 {
+    public ParticleSystem popping;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class PopBalloon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // change this so that if there is a collision, the particle plays
+        // and the balloon gets destroyed
+        if(Input.anyKeyDown) {
+            popping.Play();
+            Destroy(gameObject, 1);
+        }
     }
 }
